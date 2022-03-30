c6f554af0dd7475d1fa3becca7e1f9f899fca695
ScriptHawk <lucasrhcp50@gmail.com>
2022-03-30 19:04:40 -0300

    Adicionado o Challenge do Dia

--- 1000HelloWorld.py
+++
@@ -1,4 +0,0 @@
-#URI Hello World!
-
-
-print('Hello World!')
---
+++ ExtremamenteBasico.py
@@ -0,0 +1,8 @@
+#Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+
+
+A = int(input())
+B = int(input())
+
+X = A+B
+print(str("X = ") + str(X))
