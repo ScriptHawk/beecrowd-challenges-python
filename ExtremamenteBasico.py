ed4ff435eb9ba41320c40d5688da6edf48ed96b9
ScriptHawk <lucasrhcp50@gmail.com>
2022-03-31 18:17:50 -0300

    add Challenge of the day

--- ExtremamenteBasico.py
+++ ExtremamenteBasico.py
@@ -1,8 +1,7 @@
 #Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. 
 
-
 A = int(input())
 B = int(input())
-
 X = A+B
+
 print(str("X = ") + str(X))
