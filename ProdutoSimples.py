66cc81d03fd1d661da41658a6df8040af026d185
ScriptHawk <lucasrhcp50@gmail.com>
2022-04-04 19:10:14 -0300

    add-challenge-of-the-day

---
+++ ProdutoSimples.py
@@ -0,0 +1,11 @@
+#Leia dois valores inteiros. 
+#A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+#A seguir mostre a variável PROD com mensagem correspondente.   
+
+multiplicando = int(input())
+multiplicador = int(input())
+
+
+produto = multiplicando*multiplicador
+
+print(str("PROD = ") + str(produto))
