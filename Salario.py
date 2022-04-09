f9cb747f2ec408cd13fec2e3f141c42d7597a822
ScriptHawk <lucasrhcp50@gmail.com>
2022-04-08 22:37:10 -0300

    add-challenge-of-the-day

---
+++ Salario.py
@@ -0,0 +1,11 @@
+#Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. 
+#A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+numeroFuncionario = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+
+salarioFinal = horasTrabalhadas*valorHora
+
+print(str("NUMBER = ") + str(numeroFuncionario))
+print('SALARY = U$ %.2f'%salarioFinal)
