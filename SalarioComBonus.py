027fe457e9958dc3d2a51888b54edee70cefd901
ScriptHawk <lucasrhcp50@gmail.com>
2022-04-11 14:47:06 -0300

    add-challenge-of-the-day

---
+++ SalarioComBonus.py
@@ -0,0 +1,11 @@
+#Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). 
+#Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
+
+nomeVendedor = str(input())
+salarioFixo = float(input())
+totalVendas = float(input())
+
+totalVendas = totalVendas*0.15
+salarioFixo = totalVendas+salarioFixo
+
+print('TOTAL = R$ %.2f'%salarioFixo)
